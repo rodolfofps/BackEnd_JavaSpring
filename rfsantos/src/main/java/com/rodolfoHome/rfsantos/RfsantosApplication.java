1e94f28de358b9f7b7171b507c2f320d98c9589f
Rodolfo Santos <105891609+rodolfofps@users.noreply.github.com>
2025-01-26 18:41:21 -0300

    Project created

---
+++ rfsantos/src/main/java/com/rodolfoHome/rfsantos/RfsantosApplication.java
@@ -0,0 +1,13 @@
+package com.rodolfoHome.rfsantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfsantosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfsantosApplication.class, args);
+	}
+
+}
---
+++ rfsantos/src/test/java/com/rodolfoHome/rfsantos/RfsantosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodolfoHome.rfsantos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfsantosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
